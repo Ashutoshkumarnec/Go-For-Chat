eb38bde6360e4ccdde131ef1105097eeed6a15a2
Ashutosh <ashutoshkumarnec18@gmail.com>
2018-10-03 17:04:37 +0530

    Updates

---
+++ precache-manifest.7e043eafc781dc0f86adb4e361be6747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Go-For-Chat/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2550d91846cc4314c7ee",
+    "url": "/Go-For-Chat/static/js/runtime~main.2550d918.js"
+  },
+  {
+    "revision": "26328dbd3b1026a22d1c",
+    "url": "/Go-For-Chat/static/js/main.26328dbd.chunk.js"
+  },
+  {
+    "revision": "1f28770d49df82f969d4",
+    "url": "/Go-For-Chat/static/js/1.1f28770d.chunk.js"
+  },
+  {
+    "revision": "26328dbd3b1026a22d1c",
+    "url": "/Go-For-Chat/static/css/main.c55eb524.chunk.css"
+  },
+  {
+    "revision": "23ef55d4e17192f7fa7b7f5e50d90883",
+    "url": "/Go-For-Chat/index.html"
+  }
+];
